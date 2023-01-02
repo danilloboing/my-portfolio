d4015adb840683546a2dbd5a69201723ef1a59c8
danilloboing <danillo.boing.souza@gmail.com>
2023-01-01 22:21:53 -0300

    portfolio responsivo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Portfolio
+
+## 📑 Description
+
+This is my portfolio to show you a little bit about myself and my projects throughout my career! 
+
+## 💵 License
+
+Use of the files for purposes other than learning is forbidden.
---
+++ assets/js/Portfolio.js
@@ -0,0 +1,34 @@
+const skills = document.querySelectorAll('.skill-box');
+const descricao = document.querySelector('.texto-descricao');
+const sobreSkill = [
+                    '<p>HTML</p> <br> <p>É um bloco de construção  da web. Define o significado e a estrutura do conteúdo da web. Outras tecnologias além do HTML geralmente são usadas para descrever a aparência/apresentação (CSS) ou a funcionalidade/comportamento (JavaScript) de uma página da web.</p> <br>',
+                    '<p>CSS</p> <br> <p>Trata-se de uma linguagem de marcação, amplamente utilizada com HTML ou XHTML, e representa diversas possibilidades para a formatação. O CSS ajuda a editar, alinhar, remover e trabalhar no espaço entre elementos de uma página.</p> <br>',
+                    '<p>JavaScript</p> <br> <p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web.</p> <br>',
+                    '<p>Git</p> <br> <p>O Git é um sistema de controle de versão de código-fonte aberto. Ele facilita esse tipo de colaboração no projeto por meio do controle de versão distribuído de arquivos que residem em repositórios.</p> <br>',
+                    '<p>Github</p> <br> <p>É uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. </p> <br>'
+]
+const dataInicio = [
+                    (new Date(2021, 10, 1)).getTime(), // 0
+                    (new Date(2021, 10, 1)).getTime(), // 1
+                    (new Date(2022, 0, 1)).getTime(), // 2
+                    (new Date(2022, 3, 1)).getTime(), // 3
+                    (new Date(2022, 3, 1)).getTime(), // 4
+                    (new Date(2022, 1, 1)).getTime(), // 5
+                    (new Date(2021, 11, 1)).getTime(), // 6
+                    (new Date(2021, 11, 1)).getTime(), // 7
+                    ]
+const dataHoje = (new Date()).getTime();
+
+skills.forEach(  (elemento, index) => {
+    let index1 = index;
+    let elemento1 = elemento;
+    elemento.addEventListener('mouseover', (evento) => {
+        let tempo = 'mês';
+        let tempoDeExperiencia = Math.round((dataHoje-dataInicio[index1])/(1000*60*60*24*30));
+        if (tempoDeExperiencia > 1) tempo = 'meses'
+        descricao.innerHTML = `<p>${sobreSkill[index1]} </p>` ;
+    } )
+    elemento.addEventListener('mouseout', (evento, elemento,) => {
+        descricao.innerHTML = '/* Passe o mouse por cima de alguma habilidade para ler a descrição */';
+    } )
+} );
